77b4f0da27e631dd70abb835e634055ddff93e63
Hrvoje <hrvoje.bresic@gmail.com>
2022-01-22 16:54:13 +0100

    solved task 1

--- pytorch/moje_mreze.py
+++ pytorch/moje_mreze.py
@@ -22,3 +22,78 @@ class MyNet(nn.Module):
         x = F.relu(x)
         x = self.fc2(x)
         return x
+
+
+class VGG16(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=(3, 3), padding=1)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=(3, 3), padding=1)
+
+        self.pool = nn.MaxPool2d((2, 2))
+
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=(3, 3), padding=1)
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=(3, 3), padding=1)
+
+        self.conv5 = nn.Conv2d(128, 256, (3, 3), padding=1)
+        self.conv6 = nn.Conv2d(256, 256, (3, 3), padding=1)
+        self.conv7 = nn.Conv2d(256, 256, (3, 3), padding=1)
+
+        self.conv8 = nn.Conv2d(256, 512, (3, 3), padding=1)
+        self.conv9 = nn.Conv2d(512, 512, (3, 3), padding=1)
+        self.conv10 = nn.Conv2d(512, 512, (3, 3), padding=1)
+
+        self.conv11 = nn.Conv2d(512, 512, (3, 3), padding=1)
+        self.conv12 = nn.Conv2d(512, 512, (3, 3), padding=1)
+        self.conv13 = nn.Conv2d(512, 512, (3, 3), padding=1)
+
+        self.fc1 = nn.Linear(512, 4096)
+        self.fc2 = nn.Linear(4096, 4096)
+        self.fc3 = nn.Linear(4096, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.pool(x)
+
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = self.conv4(x)
+        x = F.relu(x)
+        x = self.pool(x)
+
+        x = self.conv5(x)
+        x = F.relu(x)
+        x = self.conv6(x)
+        x = F.relu(x)
+        x = self.conv7(x)
+        x = F.relu(x)
+        x = self.pool(x)
+
+        x = self.conv8(x)
+        x = F.relu(x)
+        x = self.conv9(x)
+        x = F.relu(x)
+        x = self.conv10(x)
+        x = F.relu(x)
+        x = self.pool(x)
+
+        x = self.conv11(x)
+        x = F.relu(x)
+        x = self.conv12(x)
+        x = F.relu(x)
+        x = self.conv13(x)
+        x = F.relu(x)
+        # x = self.pool(x)
+
+        x = x.view(-1, 512)
+
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+
+        return x
--- pytorch/zad1.py
+++ pytorch/zad1.py
@@ -2,11 +2,15 @@ from moje_mreze import MyNet
 import torch
 import cv2
 import numpy as np
+from torchsummary import summary
+import matplotlib.pyplot as plt
 
-device = 'cpu' #'cuda' if torch.cuda.is_available() else ' cpu'
+device = 'cuda' if torch.cuda.is_available() else ' cpu'
 
 model = torch.load('model.pt').to(device)
 model.eval()
+
+print(summary(model, (1, 28, 28)))
 
 orig_img = cv2.imread('test.png', cv2.IMREAD_GRAYSCALE)
 
@@ -18,7 +22,9 @@ img = img.astype('float32')
 
 img = torch.tensor(img)
 
+
 img = torch.unsqueeze(img, 0)
+
 
 img = img.to(device)
 
